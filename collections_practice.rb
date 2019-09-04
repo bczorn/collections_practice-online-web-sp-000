1e356e5489663692d8ab46d7d09a77bf73f54e5c
Becky Zorn <bczorn@gmail.com>
2019-09-04 03:03:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort do |a,b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a == b
+      0
+    elsif a < b
+      1
+    elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array = [array[0], array[2], array[1]]
+end
+
+def reverse_array(array)
+  array.reverse!
+end
+
+def kesha_maker(array)
+  
+      
+end
+
+def find_a(array)
+  
+end
+
+def sum_array(array)
+  
+end
+
+def add_s(array)
+  array.each do |x|
+    unless x = array[1]
+    x << "s"
+    end
+  end
+end
